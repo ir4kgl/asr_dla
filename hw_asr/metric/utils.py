378cfc682335aa8fa101bf28650d43bdc82395d8
Ira <vishneviyshturman@gmail.com>
2023-10-07 22:27:48 +0300

    implement metrics calculation

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,14 @@
-# Don't forget to support cases when target_text == ''
+import editdistance
+
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
-
+    if len(target_text) == 0:
+        return 1 if len(predicted_text) != 0 else 0
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    target_splitted = list(target_text.split(' '))
+    pred_splitted = list(predicted_text.split(' '))
+    if len(target_splitted) == 0:
+        return 1 if len(pred_splitted) != 0 else 0
+    return editdistance.eval(target_splitted, pred_splitted) / len(target_splitted)
