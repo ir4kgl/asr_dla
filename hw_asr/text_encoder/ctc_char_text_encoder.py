fa9555e4d9aa34e61abc032efdce5803cf630ed3
Ira <vishneviyshturman@gmail.com>
2023-10-08 00:34:25 +0300

    implement beam search and ctc decode

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,4 +1,5 @@
 from typing import List, NamedTuple
+from collections import defaultdict
 
 import torch
 
@@ -20,8 +21,15 @@ class CTCCharTextEncoder(CharTextEncoder):
         self.char2ind = {v: k for k, v in self.ind2char.items()}
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
-        raise NotImplementedError()
+        res = list()
+        PREV = 0
+        for i in inds:
+            if i == 0 or i == PREV:
+                PREV = i
+                continue
+            res.append(self.ind2char[i])
+            PREV = i
+        return ''.join(res)
 
     def ctc_beam_search(self, probs: torch.tensor, probs_length,
                         beam_size: int = 100) -> List[Hypothesis]:
@@ -32,6 +40,21 @@ class CTCCharTextEncoder(CharTextEncoder):
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
         hypos: List[Hypothesis] = []
-        # TODO: your code here
-        raise NotImplementedError
+
+        hypos.append(Hypothesis('', 1.))
+
+        for i in range(char_length):
+            new_hypos: List[Hypothesis] = []
+
+            for next_ind, next_char in self.ind2char.items():
+                for prefix in hypos:
+                    p = prefix.prob * probs[i][next_ind]
+                    if len(prefix.text) > 0 and prefix.text[-1] == next_char:
+                        new_hypos.append((prefix.text, p))
+                    elif len(prefix.text) > 0 and prefix.text[-1] == self.EMPTY_TOK:
+                        new_hypos.append((prefix.text[:-1] + next_char, p))
+                    else:
+                        new_hypos.append((prefix.text + next_char, p))
+            hypos = list(sorted(new_hypos, key=lambda x: x.prob, reverse=True)[:beam_size])
+
         return sorted(hypos, key=lambda x: x.prob, reverse=True)
