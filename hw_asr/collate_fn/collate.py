cbedf42debc752d173772fc3e390f7d4137b5c54
Ira <vishneviyshturman@gmail.com>
2023-10-10 23:48:26 +0300

    implement collate

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,5 +1,8 @@
+import torch
 import logging
 from typing import List
+
+from torch.nn import ConstantPad2d
 
 logger = logging.getLogger(__name__)
 
@@ -10,5 +13,18 @@ def collate_fn(dataset_items: List[dict]):
     """
 
     result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+    result_batch["text"] = list(x["text"] for x in dataset_items)
+    result_batch["text_encoded"] = list(
+        x["text_encoded"] for x in dataset_items)
+    result_batch["duration"] = list(x["duration"] for x in dataset_items)
+    result_batch["audio_path"] = list(x["audio_path"] for x in dataset_items)
+
+    pad_audio = max(list(x["audio"].shape[-1] for x in dataset_items))
+    pad_spec = max(list(x["spectrogram"].shape[-1] for x in dataset_items))
+
+    result_batch["audio"] = torch.stack(
+        tuple(ConstantPad2d((0, pad_audio - x["audio"].shape[-1], 0, 0), 0)(x["audio"]) for x in dataset_items))
+    result_batch["spectrogram"] = torch.stack(
+        tuple(ConstantPad2d((0, pad_spec - x["spectrogram"].shape[-1], 0, 0), 0)(x["spectrogram"]) for x in dataset_items))
+
+    return result_batch
