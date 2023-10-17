24954545d8aa8f7f46224946ee71d7349bf43b30
Ira <vishneviyshturman@gmail.com>
2023-10-17 21:43:32 +0300

    init

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -1,6 +1,6 @@
 from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
-from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
+from hw_asr.datasets.librispeech_dataset import LibrispeechDataset, LibrispeechDatasetKaggle
 from hw_asr.datasets.ljspeech_dataset import LJspeechDataset
 from hw_asr.datasets.common_voice import CommonVoiceDataset
 
@@ -9,5 +9,6 @@ __all__ = [
     "CustomDirAudioDataset",
     "CustomAudioDataset",
     "LJspeechDataset",
-    "CommonVoiceDataset"
+    "CommonVoiceDataset",
+    "LibrispeechDatasetKaggle"
 ]
