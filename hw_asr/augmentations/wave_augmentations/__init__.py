01737c2596d9cb6fe714e9312773f73a960fe6c7
Ira <vishneviyshturman@gmail.com>
2023-10-17 23:47:46 +0300

    add wave augs

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,9 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.Pitchshift import PitchShift
+from hw_asr.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "PitchShift",
+    "AddBackgroundNoise"
 ]
