a9a6eb5eb483242f5491ecf7af59030abc5da667
Ira <vishneviyshturman@gmail.com>
2023-10-18 00:31:38 +0300

    del bg noise

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,9 +1,7 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
 from hw_asr.augmentations.wave_augmentations.Pitchshift import PitchShift
-from hw_asr.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
 
 __all__ = [
     "Gain",
-    "PitchShift",
-    "AddBackgroundNoise"
+    "PitchShift"
 ]
