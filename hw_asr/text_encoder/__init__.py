3b6049dcb5ec96ab0e1d2f32835c77a94de08d79
ir4kgl <vishneviyshturman@gmail.com>
2024-01-21 22:27:05 +0300

    rename configs

--- hw_asr/text_encoder/__init__.py
+++ hw_asr/text_encoder/__init__.py
@@ -1,10 +1,8 @@
 from .char_text_encoder import CharTextEncoder
 from .ctc_char_text_encoder import CTCCharTextEncoder, CTCCharTextEncoderWithLM
-from .ctc_bytepair_text_encoder import CTCBPE
 
 __all__ = [
     "CharTextEncoder",
     "CTCCharTextEncoder",
     "CTCCharTextEncoderWithLM",
-    "CTCBPE"
 ]
